303dcd39a1a443e28b48cc5401943325ba66faa2
froxyroar <120387488+froxyroar@users.noreply.github.com>
2023-12-16 19:51:40 +0700

    first
    last

---
+++ RentalKostum/src/rentalkostum/RentalKostum.java
@@ -0,0 +1,16 @@
+package rentalkostum;
+
+/**
+ *
+ * @author rifki
+ */
+public class RentalKostum {
+    public static void main(String[] args) {
+        Login lg = new Login ();
+        lg.setVisible(true);
+        lg.pack();
+        lg.setLocationRelativeTo(null);
+        lg.setDefaultCloseOperation(Login.EXIT_ON_CLOSE);
+    }
+    
+}
